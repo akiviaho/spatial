d41d468376e8444628ee360a47b149ce23b0bc1a
Antti Kiviaho <ak431480@narvi-shell.cc.tut.fi>
2023-06-20 10:46:24 +0300

    Completely overhauled the nmf process. Implemented it separately for each sample and followed the whole pipeline outlined by Barkley et al.

--- scripts/nmf-on-celltype.py
+++ scripts/nmf-on-celltype.py
@@ -1,19 +1,31 @@
 import os 
 os.chdir('/lustre/scratch/kiviaho/prostate_spatial')
 import numpy as np
+import pandas as pd
 import scanpy as sc
-import scib
-#from sklearn.decomposition import NMF
+from itertools import product
 import nimfa
-import upsetplot as ups
-from matplotlib import pyplot as plt
 import argparse
+
 
 def save_to_pickle(obj,filename):
     import pickle
     with open(filename, 'wb') as handle:
         pickle.dump(obj, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+def nmf_preprocessing(dat,n_var_genes=2000):
+
+    dat.X = dat.layers['counts'].copy()
+    sc.pp.filter_genes(dat,min_counts=1)
+    sc.pp.normalize_total(dat)
+    sc.pp.scale(dat)
+    sc.pp.highly_variable_genes(dat,n_top_genes=n_var_genes,flavor='seurat_v3',
+                                subset=True, layer='counts')
+
+    # Replace negative entries with zero
+    dat.X[dat.X < 0] = 0
+
+    return(dat)
 
 def calculate_matrix_orders(arr):
     # get the indices that would sort each row of the array
@@ -30,81 +42,165 @@ def calculate_matrix_orders(arr):
     return result
 
 
-### Params ###
+# Helper function to calculate the Jaccard index of two lists
+def jaccard(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    intersection = set1.intersection(set2)
+    union = set1.union(set2)
+    return len(intersection)/len(union)
+
+
+################# Params ########################
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--filename', type=str, help='Name of the file to be used')
 parser.add_argument('--n_var_genes', type=int,default=2000, help='The target number of HVGs')
 args = parser.parse_args()
 
 filename = args.filename
-n_var_genes = args.n_var_genes
-n_comps_range =  np.arange(5,26)
+n_hvgs = args.n_var_genes
+min_cells = 100
+nmf_comp_range = np.arange(5,26)[::-1]
 
-path_for_plots = 'sc_modules/plots/'
-path_for_data = 'sc_modules/data/'
+if __name__ == '__main__':
 
-### Read and process the cell type subsetted data ###
+    # Download data
+    ctype_dat = sc.read_h5ad(filename)
 
-dat = sc.read_h5ad(filename)
-dat.X = dat.layers['counts'].copy()
+    # Initialize dict objects
+    nmf_sample_dict = {}
+    dq_samples = {}
 
+    ################# First part: calculating NMFs ########################
 
-sc.pp.filter_genes(dat,min_counts=1)
-sc.pp.normalize_total(dat)
-sc.pp.scale(dat)
-sc.pp.highly_variable_genes(dat,n_top_genes=n_var_genes,flavor='seurat_v3',
-                            subset=True, layer='counts')
+    for s in np.unique(ctype_dat.obs['sample']):
 
-# Replace negative entries with zero
-dat.X[dat.X < 0] = 0
+        if len(ctype_dat.obs[ctype_dat.obs['sample']==s]) >= min_cells:
 
-### Calulating the NMF ###
+            # Subset the data to only include a single sample
+            dat = ctype_dat[ctype_dat.obs['sample'] == s]
+            
+            # Do a sample-specific preprocessing
+            dat = nmf_preprocessing(dat,n_hvgs)
 
-for n_comps in n_comps_range:
+            for n_comps in nmf_comp_range:
 
-#    model = NMF(n_components=n_comps, init='nndsvda', solver='mu', 
-#                beta_loss='kullback-leibler',random_state=35735)
-#    W = model.fit_transform(dat.X)
-#    H = model.components_
+                # Perform non-negative matrix factorization using nsNMF
+                f = nimfa.Nsnmf(dat.X, rank=n_comps)
+                f_fit = f()
+                    
+                # Extract the resulting matrices into variables W and H
+                W = np.array(f_fit.basis())
+                H = np.array(f_fit.coef())
 
+                res = {'samples':dat.obs,'genes':dat.var,'sample_weights':W,'gene_weights':H}
 
-    # Perform non-negative matrix factorization using nsNMF
-    f = nimfa.Nsnmf(dat.X, rank=n_comps)
-    f_fit = f()
-        
-    # Extract the resulting matrices into variables W and H
-    W = np.array(f_fit.basis())
-    H = np.array(f_fit.coef())
-    
-    res = {'samples':dat.obs,'genes':dat.var,'sample_weights':W,'gene_weights':H}
+                gene_w = res['gene_weights'].T
+                genes = list(res['genes'].index)
 
-    save_to_pickle(res,path_for_data+filename.replace('.h5ad','_'+str(n_comps)+'_modules_nsnmf_results.pickle'))
+                mat_1 = calculate_matrix_orders(gene_w.T).T
+                mat_2 = calculate_matrix_orders(gene_w)
 
-    gene_w = res['gene_weights'].T
-    genes = list(res['genes'].index)
+                # Iterate through the factors (columns)
+                genes_by_factors = {}
+                for factor in range(gene_w.shape[1]):
+                    valid_genes = list()
+                    # Iterate through the genes, starting from the highest weighted gene of this factor
+                    for i in np.argsort(mat_1[:,factor]): 
+                        if mat_2[i,factor] ==0: # Is this the factor the gene effects the most?
+                            
+                            # If yes, add it to the list of genes
+                            valid_genes.append(genes[i])
+                        else:
+                            # if not, stop adding genes into the list, move on to the next factor
+                            break 
+                    # Append the list of valid genes into a dictionary under the appropriate key
+                    genes_by_factors['factor'+str(factor)] = valid_genes
 
-    mat_1 = calculate_matrix_orders(gene_w.T).T
-    mat_2 = calculate_matrix_orders(gene_w)
-
-    # Iterate through the factors (columns)
-    genes_by_factors = {}
-    for factor in range(gene_w.shape[1]):
-        valid_genes = list()
-        # Iterate through the genes, starting from the highest weighted gene of this factor
-        for i in np.argsort(mat_1[:,factor]): 
-            if mat_2[i,factor] ==0: # Is this the factor the gene effects the most?
                 
-                # If yes, add it to the list of genes
-                valid_genes.append(genes[i])
-            else:
-                # if not, stop adding genes into the list, move on to the next factor
-                break 
-        # Append the list of valid genes into a dictionary under the appropriate key
-        genes_by_factors['factor'+str(factor)] = valid_genes
+                all_lists_have_at_least_five = True
 
+                for lst in genes_by_factors.values():
+                    if len(lst) < 5:
+                        all_lists_have_at_least_five = False
+                        break
 
-    ups_data = ups.from_contents(genes_by_factors)
-    ax_dict = ups.UpSet(ups_data, subset_size='count',show_counts=True).plot()
+                if all_lists_have_at_least_five:
+                    print(s+": valid factors (min. 5 genes) when using n_comps="+str(n_comps))
+                    nmf_sample_dict[s] = genes_by_factors
+                    break
+        else:
+            dq_samples[s] = len(ctype_dat.obs[ctype_dat.obs['sample']==s])
+        
+    for k in list(dq_samples.keys()):
+        print(k + ' not processed for too few cells (' + str(dq_samples[k])+')')
 
-    plt.savefig(path_for_plots+'upsetplot_'+filename.replace('.h5ad','')+'_'+str(n_comps)+'_modules.png')
+    ################# Second part: Observing module overlaps ########################
+
+    # A dictionary to keep track of overlapping factors
+    overlapping_factors = {}
+
+    # Go through each sample factor combination
+    for (sample1, factor1), (sample2, factor2) in product([(sample, factor) for sample in nmf_sample_dict for factor in nmf_sample_dict[sample]], repeat=2):
+        # Ignore if it's the same sample and factor combination
+        if sample1 == sample2 and factor1 == factor2:
+            continue
+        # Calculate the Jaccard index of the two lists
+        jaccard_index = jaccard(nmf_sample_dict[sample1][factor1], nmf_sample_dict[sample2][factor2])
+        # If the overlap is more than 5%, add it to the overlapping_factors dictionary
+        if jaccard_index > 0.05:
+            overlapping_factors.setdefault((sample1, factor1), set()).add((sample2, factor2))
+            
+    # A set to keep track of factors with more than two overlaps
+    valid_factors = set()
+
+    # Go through each factor
+    for key, value in overlapping_factors.items():
+        # If the factor overlaps with at least two other factors, add it to the valid_factors set
+        if len(value) >= 2:
+            valid_factors.add(key)
+
+    # A dictionary to keep track of how many overlapping factors each gene has
+    gene_overlaps = {}
+
+    # Go through each valid factor
+    for factor in valid_factors:
+        # Get the sample and factor
+        sample, factor_name = factor
+        # Go through each gene in the factor
+        for gene in nmf_sample_dict[sample][factor_name]:
+            # Add one to the gene's overlaps count
+            gene_overlaps[gene] = gene_overlaps.get(gene, 0) + 1
+            
+    # Create an empty 2D array to serve as the adjacency matrix
+    adj_matrix = [[0 for gene2 in gene_overlaps.keys()] for gene1 in gene_overlaps.keys()]
+
+    # Loop through each valid factor
+    for factor in valid_factors:
+        # Get the sample and factor
+        sample, factor_name = factor
+        
+        # Loop through each gene in the factor
+        for gene1 in nmf_sample_dict[sample][factor_name]:
+            # Loop through each other gene in the same factor
+            for gene2 in nmf_sample_dict[sample][factor_name]:
+                # Ignore if it's the same gene
+                if gene1 == gene2:
+                    continue
+                # Increment the value for this pair of genes in the adjacency matrix
+                adj_matrix[list(gene_overlaps.keys()).index(gene1)][list(gene_overlaps.keys()).index(gene2)] += 1
+
+    adj_df = pd.DataFrame(adj_matrix,columns=list(gene_overlaps.keys()),index=list(gene_overlaps.keys()))
+
+    # Filter connections occurring in fewer than 2 modules
+    adj_df = adj_df[adj_df.max(axis=1) >= 2]
+
+    # Filter rows with more than 3 non-zero entries
+    adj_df = adj_df[adj_df.astype(bool).sum(axis=1) >= 3]
+
+    # Save the adjacency matrix to a csv
+    save_file = filename.replace('.h5ad','_nmf_derived_gene_adjacencies.csv')
+    adj_df.to_csv(save_file)
+
 
--- scripts/run-nmf.sh
+++ scripts/run-nmf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -t 1-23:59:00
-#SBATCH -J celltype-nmf
-#SBATCH -o celltype-nmf.out.%j
-#SBATCH -e celltype-nmf.err.%j
+#SBATCH -J Fibroblast_muscle-nmf
+#SBATCH -o Fibroblast_muscle-nmf.out.%j
+#SBATCH -e Fibroblast_muscle-nmf.err.%j
 #SBATCH --partition=normal
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=100G
+#SBATCH --mem=30G
 #SBATCH --mail-type=END
 #SBATCH --mail-user=antti.kiviaho@tuni.fi
 
@@ -16,5 +16,5 @@ source activate scib-pipeline-R4.0
 echo "conda activated"
 
 python -u ./scripts/nmf-on-celltype.py \
---filename Myeloid.h5ad \
+--filename Fibroblast_muscle.h5ad \
 --n_var_genes 2000
