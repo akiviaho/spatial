352e426c87ba1593530bba687bf369317b266f29
Antti Kiviaho <ak431480@narvi-shell.cc.tut.fi>
2024-06-04 09:00:26 +0300

    Status at submission

--- scripts/spaceranger-script.sh
+++ scripts/spaceranger-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --time=48:00:00
+#SBATCH --time=5-23:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=100G
 #SBATCH --partition=normal
-#SBATCH --job-name=V12N14-331-slide
-#SBATCH --output=V12N14-331-spaceranger.out
+#SBATCH --job-name=V13Y08-090-slide
+#SBATCH --output=V13Y08-090-spaceranger.out
 #SBATCH --mail-user=antti.kiviaho@tuni.fi
 #SBATCH --mail-type=END
 
@@ -14,9 +14,9 @@
 # Batch script to run spaceranger on a node
 # Refer to sample-info.xlsx for variables (in Dropbox)
 
-SLIDE=V12N14-331
-SAMPLES=(PC_06_04077_OIK_ANT_2 PC_06_04581_OIK_POST_0 PC_06_16086_VAS_POST_2)
-AREAS=(A1 B1 D1)
+SLIDE=V13Y08-090
+SAMPLES=(MET_A3 MET_GP12 MET_A14 MET_A16)
+AREAS=(A1 B1 C1 D1)
 
 module purge
 module load compbio/spaceranger
@@ -26,7 +26,7 @@ for i in "${!SAMPLES[@]}"; do
   AREA="${AREAS[$i]}"
   spaceranger count --id="${SAMPLE}" \
   --transcriptome=/lustre/scratch/kiviaho/prostate_spatial/data/refdata-cellranger-GRCh38-3.0.0/ \
-  --fastqs=/lustre/scratch/kiviaho/prostate_spatial/data/reads/2023-reads/${SAMPLE} \
+  --fastqs=/lustre/scratch/kiviaho/prostate_spatial/data/reads/2024-met-reads/X208SC24033399-Z01-F001/01.RawData/${AREA}_${SAMPLE} \
   --sample="${SAMPLE}" \
   --image="/lustre/scratch/kiviaho/prostate_spatial/data/images/${SAMPLE}_20x_zoom.tiff" \
   --slide=$SLIDE \
